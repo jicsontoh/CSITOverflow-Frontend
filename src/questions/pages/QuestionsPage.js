380dcf0d173c22e3d9ef26e735aebf18c0f3dd57
jicsontoh <jicsontoh@gmail.com>
2023-08-13 15:24:28 +0800

    Add questions tabs

--- src/questions/pages/QuestionsPage.js
+++ src/questions/pages/QuestionsPage.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import SideBar from "../../shared/components/SideBar";
+import ButtonGroup from "../../shared/buttons/ButtonGroup";
+
+import "./QuestionsPage.css";
 
 const QuestionsPage = (props) => {
+  const [sortType, setSortType] = useState("Newest");
+
   return (
     <React.Fragment>
       <div className="page">
         <SideBar />
-        <div id="mainbar" className="homepage">
+        <div id="mainbar" className="questions-page">
           <div className="questions-grid">
             <h3 className="questions-headline">All Questions</h3>
             <NavLink to="/questions/new" className="ask-qns-button">
               Ask Question
             </NavLink>
+          </div>
+          <div className="questions-tabs">
+            <ButtonGroup
+              buttons={["Newest", "Top", "Views", "Oldest"]}
+              selected={sortType}
+              setSelected={setSortType}
+            />
           </div>
         </div>
       </div>
