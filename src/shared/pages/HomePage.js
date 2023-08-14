4ab3c662538fe680b79663b0ec1160d63a3b1daa
jicsontoh <jicsontoh@gmail.com>
2023-08-14 16:49:08 +0800

    Update HomePage

--- src/shared/pages/HomePage.js
+++ src/shared/pages/HomePage.js
@@ -2,8 +2,54 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import SideBar from "../components/SideBar";
+import QuestionList from "../../questions/components/QuestionList";
 
 import "./HomePage.css";
+
+const posts = [
+  {
+    id: 1,
+    title: "qns title",
+    body: "question body",
+    username: "username",
+    gravatar: "image",
+    user_id: "userid1",
+    answer_count: 10,
+    comment_count: 20,
+    views: 27,
+    votes: 10,
+    created_at: "2023/08/14, 13:00",
+    tags: "tags",
+  },
+  {
+    id: 2,
+    title: "qns title2",
+    body: "question body222",
+    username: "username",
+    gravatar: "image",
+    user_id: "userid1",
+    answer_count: 10,
+    comment_count: 20,
+    views: 30,
+    votes: 11,
+    created_at: "2023/07/14, 13:00",
+    tags: "tags",
+  },
+  {
+    id: 3,
+    title: "qns title3",
+    body: "question body333",
+    username: "username2",
+    gravatar: "image",
+    user_id: "userid2",
+    answer_count: 5,
+    comment_count: 10,
+    views: 24,
+    votes: 15,
+    created_at: "2023/06/14, 13:00",
+    tags: "tags",
+  },
+];
 
 const HomePage = (props) => {
   return (
@@ -17,6 +63,8 @@ const HomePage = (props) => {
               Ask Question
             </NavLink>
           </div>
+
+          <QuestionList items={posts} sortType="Top" />
         </div>
       </div>
     </React.Fragment>
