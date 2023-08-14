4c39367b0b740754a7fb6d1a5598bc5b9a754e87
jicsontoh <jicsontoh@gmail.com>
2023-08-14 11:48:34 +0800

    Update Tag Page

--- src/tags/pages/TagPage.js
+++ src/tags/pages/TagPage.js
@@ -2,14 +2,22 @@ import React from "react";
 
 import SideBar from "../../shared/components/SideBar";
 
+import "./TagPage.css";
+
 const TagPage = (props) => {
   return (
     <React.Fragment>
       <div className="page">
         <SideBar />
-        <div id="mainbar" className="homepage">
+        <div id="mainbar" className="tags-page fc-black-800">
           <div className="questions-grid">
-            <h3 className="questions-headline">Tags</h3>
+            <h3 className="headline">Tags</h3>
+            <p className="fs-body">
+              A tag is a keyword or label that categorizes your question with
+              other, similar questions. <br />
+              Using the right tags makes it easier for others to find and answer
+              your question.
+            </p>
           </div>
         </div>
       </div>
