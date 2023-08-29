b4d6e89d779e19388fb06b098879e82bde1c02b7
jicsontoh <jicsontoh@gmail.com>
2023-08-29 09:18:39 +0800

    Update question page

--- src/questions/components/QuestionItem.js
+++ src/questions/components/QuestionItem.js
@@ -16,53 +16,11 @@ import AnswerSection from "../../answers/components/AnswerSection";
 import LoadingSpinner from "../../shared/UIElements/LoadingSpinner";
 import ErrorModal from "../../shared/UIElements/ErrorModal";
 import { useHttpClient } from "../../shared/hooks/http-hook";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 
 import "./QuestionItem.css";
-
-// const posts = [
-//   {
-//     id: "1",
-//     title: "qns title",
-//     body: "question body",
-//     username: "username",
-//     gravatar: "image",
-//     user_id: "userid1",
-//     answer_count: 10,
-//     comment_count: 20,
-//     views: 27,
-//     votes: 10,
-//     created_at: "2023/08/14, 13:00",
-//     tags: "tags",
-//   },
-//   {
-//     id: "2",
-//     title: "qns title2",
-//     body: "question body222",
-//     username: "username",
-//     gravatar: "image",
-//     user_id: "userid1",
-//     answer_count: 10,
-//     comment_count: 20,
-//     views: 30,
-//     votes: 11,
-//     created_at: "2023/07/14, 13:00",
-//     tags: "tags",
-//   },
-//   {
-//     id: "3",
-//     title: "qns title3",
-//     body: "question body333",
-//     username: "username2",
-//     gravatar: "image",
-//     user_id: "userid2",
-//     answer_count: 5,
-//     comment_count: 10,
-//     views: 24,
-//     votes: 15,
-//     created_at: "2023/06/14, 13:00",
-//     tags: "tags",
-//   },
-// ];
 
 const QuestionItem = (props) => {
   const auth = useContext(AuthContext);
@@ -190,7 +148,15 @@ const QuestionItem = (props) => {
                     </div>
                   )}
 
-                  <div className="question-body">{loadedQns.body}</div>
+                  <div className="question-body">
+                    <LexicalComposer
+                      initialConfig={{
+                        editorState: loadedQns.body,
+                      }}
+                    >
+                      <RichTextPlugin contentEditable={<ContentEditable />} />
+                    </LexicalComposer>
+                  </div>
                   <div className="post-cell">
                     <div className="post-actions fc-black-800">
                       <div className="post-actions-extended">
