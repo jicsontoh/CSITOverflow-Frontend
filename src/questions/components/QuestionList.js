ace10d9bf1f080e0f3f1f8e79118080bbc199f6f
jicsontoh <jicsontoh@gmail.com>
2023-09-20 11:14:25 +0800

    Add pagination for questions

--- src/questions/components/QuestionList.js
+++ src/questions/components/QuestionList.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PostItem from "./PostItem";
 import handleSorting from "../../shared/util/handleSorting";
+import Pagination from "../../shared/components/Pagination";
 
-// const itemsPerPage = 10;
+const itemsPerPage = 10;
 
 const QuestionList = (props) => {
+  const [page, setPage] = useState(1);
+
+  const handlePaginationChange = (e, value) => setPage(value);
+
   if (props.items.length === 0) {
     return (
       <div className="questions-grid">
@@ -15,23 +20,37 @@ const QuestionList = (props) => {
   }
 
   return (
-    <div className="questions">
-      {props.items.sort(handleSorting(props.sortType)).map((post) => (
-        <PostItem
-          key={post.id}
-          id={post.id}
-          title={post.title}
-          body={post.body}
-          username={post.username}
-          gravatar={post.gravatar}
-          user_id={post.user_id}
-          answer_count={post.answers.length}
-          votes={post.up_votes.length - post.down_votes.length}
-          created_at={post.created_at}
-          tags={post.tags}
-        />
-      ))}
-    </div>
+    <React.Fragment>
+      <div className="questions">
+        {props.items
+          .sort(handleSorting(props.sortType))
+          .slice(
+            (page - 1) * itemsPerPage,
+            (page - 1) * itemsPerPage + itemsPerPage
+          )
+          .map((post) => (
+            <PostItem
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              body={post.body}
+              username={post.username}
+              gravatar={post.gravatar}
+              user_id={post.user_id}
+              answer_count={post.answers.length}
+              votes={post.up_votes.length - post.down_votes.length}
+              created_at={post.created_at}
+              tags={post.tags}
+            />
+          ))}
+      </div>
+      <Pagination
+        page={page}
+        itemList={props.items}
+        itemsPerPage={itemsPerPage}
+        handlePaginationChange={handlePaginationChange}
+      />
+    </React.Fragment>
   );
 };
 
